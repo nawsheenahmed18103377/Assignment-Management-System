e48eaafe840cb481df9c02fbe4c20a041c9552b5
Nawsheen Ahmed <nawsheen18103377@gmail.com>
2021-12-30 10:40:59 +0600

    intial commit

---
+++ src/client/util/ClientConfig.java
@@ -0,0 +1,8 @@
+package client.util;
+
+public class ClientConfig {
+
+    public static String host = "127.0.0.1";
+    public static Integer port = 40462;
+    public static String fileRecivePath = "F:/FileRecive/files/assignments/";
+}
